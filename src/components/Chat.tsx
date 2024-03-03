5654f2dc8a07ede62ae78c95a9e646c03d2cd07b
Lucas <lucassavioli@gmail.com>
2024-03-02 21:41:17 -0300

    Implemented useChat on the frontend

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -9,37 +9,44 @@ import { useChat } from "ai/react";
 
 export function Chat() {
 
-    const { messages, input, handleInputChange, handleSubmit } = useChat();
-    
+  const { messages, input, handleInputChange, handleSubmit } = useChat();
+
   return (
     <Card className="w-[500px] h-[780px] grid grid-rows-[min-content_1fr_min-content]">
       <CardHeader>
         <CardTitle>How can I help you?</CardTitle>
         <CardDescription>Hey! How can I help you?</CardDescription>
       </CardHeader>
-      <CardContent className="space-y-4">
-        <div className="flex gap-3 text-slate-600 text-sm">
-          <Avatar>
-            <AvatarFallback>U</AvatarFallback>
-          </Avatar>
-          <p className="leading-relaxed">
-            <span className="font-bold block text-slate-700">User:</span>
-            Let's have a conversation in english
-          </p>
-        </div>
-        <div className="flex gap-3 text-slate-600 text-sm">
-          <Avatar>
-            <AvatarFallback>U</AvatarFallback>
-          </Avatar>
-          <p className="leading-relaxed">
-            <span className="font-bold block text-slate-700">AI:</span>
-            Sure what would you like to talk about?
-          </p>
-        </div>
+      <CardContent className="space-y-4">        
+      { messages.map((message) => {
+          return (
+            <div key={message.id} className="flex gap-3 text-slate-600 text-sm">
+                { message.role === "user" && (
+                <Avatar>
+                    <AvatarFallback>U</AvatarFallback>
+                </Avatar>
+                )}
+
+                { message.role === "assistant" && (
+                <Avatar>
+                    <AvatarFallback>AI</AvatarFallback>
+                </Avatar>
+                )}
+                <p className="leading-relaxed">
+                    <span className="font-bold block text-slate-700">{message.role === "user" ? "You" : "AI"}</span>
+                    {message.content}
+                </p>
+            </div>            
+          );
+        })
+      } 
+        
       </CardContent>
-      <CardFooter className="space-x-2">
-        <Input placeholder="Type your question here" />
-        <Button>Send</Button>
+      <CardFooter>
+        <form className="w-full flex gap-2" onSubmit={handleSubmit}>
+            <Input value={input} onChange={handleInputChange} placeholder="Type your question here" />
+            <Button>Send</Button>
+        </form>
       </CardFooter>
     </Card>
   );
